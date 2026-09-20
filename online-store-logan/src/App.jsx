c8fab4981cb955cfdfbcc867bb9753c4849ccca1
lbell13131313-sudo <lbell13131313@gmail.com>
2026-09-20 19:03:58 -0400

    oops

--- online-store-logan/src/App.jsx
+++ online-store-logan/src/App.jsx
@@ -7,9 +7,9 @@ function App() {
       <h2>Featured Products</h2>
 
       <ProductCard
-        name=""
-        description=""
-        price=""
+        name="Xbox Series X|S Controller"
+        description="Game controller usable for Xbox Series X|S, PC, and Phone"
+        price="$53.99"
         image="https://placehold.co/600x400"
       />
 
