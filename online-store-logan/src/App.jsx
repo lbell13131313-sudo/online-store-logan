f765a15efacb5fecb1ed017476245733764d7635
lbell13131313-sudo <lbell13131313@gmail.com>
2026-09-20 21:38:23 -0400

    improved the product cards appearance

--- online-store-logan/src/App.jsx
+++ online-store-logan/src/App.jsx
@@ -19,27 +19,29 @@ function App() {
 
       <h3>Featured Products</h3>
 
-      {/* supplies the props to the ProductCard function in ProductCard.jsx */}
-      <ProductCard
-        name="Xbox Series X|S Controller"
-        description="Game controller usable for Xbox Series X|S, PC, and Phone"
-        price="$53.99"
-        image="https://placehold.co/300x200"
-      />
+      <div className="product_row">
+        {/* supplies the props to the ProductCard function in ProductCard.jsx */}
+        <ProductCard
+          name="Xbox Series X|S Controller"
+          description="Game controller usable for Xbox Series X|S, PC, and Phone"
+          price="$53.99"
+          image="https://placehold.co/300x200"
+        />
 
-      <ProductCard
-        name="80 Minute CD-Rs"
-        description="10 pack of writeable CD-R discs"
-        price="$7.99"
-        image="https://placehold.co/300x200"
-      />
+        <ProductCard
+          name="80 Minute CD-Rs"
+          description="10 pack of writeable CD-R discs"
+          price="$7.99"
+          image="https://placehold.co/300x200"
+        />
 
-      <ProductCard
-        name="Vinyl Player"
-        description="Plays both fullsize vinyls and mini vinyls"
-        price="$249.00"
-        image="https://placehold.co/300x200"
-      />
+        <ProductCard
+          name="Vinyl Player"
+          description="Plays both fullsize vinyls and mini vinyls"
+          price="$249.00"
+          image="https://placehold.co/300x200"
+        />
+      </div>
 
       <Footer
         store_name="Logan's Tech Shop"
